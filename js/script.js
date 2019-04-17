a65243c81f994314117418d7e03ec911e9d358a8
alexandra <le.onze.mai@gmail.com>
2019-04-17 23:48:32 +0300

    corrected pr

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
 $(document).ready(function() {
-    $('.js-slider').slick({
-        dots: true,
-        arrows: false
-    });
+  $('.js-responsive-slider').slick({
+    dots: true,
+    arrows: false
+  });
 });
 
+$(document).ready(function() {
+  $('.js-portfolio-slider').slick({
+    dots: true,
+    arrows: false
+  });
+});
+
+$(document).ready(function() {
+  $('.js-team-slider').slick({
+    dots: true,
+    arrows: false
+  });
+});
 
 
