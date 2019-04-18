499ba09ef76856c225ca6f0a1ad116eb6e531b5f
alexandra <le.onze.mai@gmail.com>
2019-04-18 15:35:38 +0300

    smoothAnchor added

--- js/script.js
+++ js/script.js
@@ -14,3 +14,20 @@ $(document).ready(function() {
     arrows: false
   });
 });
+
+function smoothAnchor() {
+  var menu = $('.js-nav');
+  var smoothSpeed = 5000;
+
+  menu.on('click','a', function (event) {
+    event.preventDefault();
+
+    var target = $(event.target);
+    var id  = target.attr('href');
+    var top = $(id).offset().top;
+
+    $('body,html').animate({scrollTop: top}, smoothSpeed);
+  });
+}
+
+smoothAnchor()
