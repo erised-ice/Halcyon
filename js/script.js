6b32cd2fe444585873002912ad35a726126eecb8
alexandra <le.onze.mai@gmail.com>
2019-04-30 00:45:58 +0300

    made adaptive for portfolio, footer

--- js/script.js
+++ js/script.js
@@ -6,7 +6,27 @@ $(document).ready(function() {
 
   $('.js-portfolio-slider').slick({
     dots: true,
-    arrows: false
+    arrows: false,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
 
   $('.js-team-slider').slick({
