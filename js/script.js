20f4d0f82c992246ae7e75de188c4efb228ad92b
alexandra <le.onze.mai@gmail.com>
2019-04-30 01:11:05 +0300

    made adaptive for team

--- js/script.js
+++ js/script.js
@@ -31,7 +31,27 @@ $(document).ready(function() {
 
   $('.js-team-slider').slick({
     dots: true,
-    arrows: false
+    arrows: false,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
 });
 
