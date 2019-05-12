2fc911f3519144e27a39964a684e139494b5b2be
alexandra <le.onze.mai@gmail.com>
2019-05-12 20:29:38 +0300

    corrected adaptive in header, slogan, profile, footer

--- js/script.js
+++ js/script.js
@@ -34,7 +34,7 @@ $(document).ready(function() {
     arrows: false,
     slidesToShow: 3,
     slidesToScroll: 3,
-    responsive: [
+    responsive: [2
       {
         breakpoint: 1024,
         settings: {
